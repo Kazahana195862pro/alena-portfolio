b10ae60287c8c3e5d3a8ada33149f5bfc465ba00
Tolochko Alena <290802823+Kazahana195862pro@users.noreply.github.com>
2026-06-05 16:28:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
     langOptions.forEach(option => {
         option.addEventListener('click', () => {
-            // Убираем активный класс у всех и добавляем кликнутому
+            // Убираем активный класс у всех переключателей и добавляем кликнутому
             langOptions.forEach(opt => opt.classList.remove('header_option_active'));
             option.classList.add('header_option_active');
 
             const selectedLang = option.getAttribute('data-lang');
 
-            // Меняем текст на выбранный язык
+            // Меняем текст на выбранный язык с поддержкой переносов строк <br>
             translatableElements.forEach(elem => {
                 const translation = elem.getAttribute(`data-${selectedLang}`);
                 if (translation) {
-                    elem.textContent = translation;
+                    // innerHTML бережно обрабатывает тег <br>, который мы добавили в HTML
+                    elem.innerHTML = translation; 
                 }
             });
         });
     });
 });
+
 
 
